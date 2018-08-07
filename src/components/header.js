7f4b3506eaa1d100551b9df92a6883deaa746ee1
leachjustin18 <leachjustin18@yahoo.com>
2018-08-06 20:38:30 -0500

    remove unused file.  Swamped out to use class for components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-);
+class Header extends React.PureComponent {
+  render() {
+    const { siteTitle } = this.props;
+    return (
+      <header>
+        <h1>
+          <Link to="/">{siteTitle}</Link>
+        </h1>
+      </header>
+    );
+  }
+}
 
 export default Header;
